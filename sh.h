67e5d292552597f55465467c3128ddc38c2147a4
Jared Glaser <jared@glaserfamily.net>
2019-03-28 20:13:35 -0400

    proper header file for print and set environment

--- sh.h
+++ sh.h
@@ -8,12 +8,14 @@ char* whichRet(char *command, struct pathelement *pathlist);
 char *where(char *command, struct pathelement *pathlist);
 void printWorkingDir();
 void list ( char **args );
-void printenv(char **envp);
+void printenv(char **envp, char **args,int forcePrintAll);
+void setEnviornment(char **envp, char**args, struct pathelement *pathlist);
 void changeDir(char **args);
 void nullify(char **args);
 void printPid();
 void promptCmd(char **args, char* prompt);
 void killProc(char **args);
+
 
 #define PROMPTMAX 32
 #define MAXARGS 10
